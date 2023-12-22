25a9bfc04d6d366a6c7e5f8a8073f26da473dfda
Antonio Pisani <antonio.pisani@gmail.com>
2023-12-22 15:47:28 -0500

    core[minor]: add new test cases for json json outputparser

--- libs/core/tests/unit_tests/output_parsers/test_json.py
+++ libs/core/tests/unit_tests/output_parsers/test_json.py
@@ -198,6 +198,8 @@ TEST_CASES_PARTIAL = [
     ('{"foo": "bar", "bar": "foo}', '{"foo": "bar", "bar": "foo}"}'),
     ('{"foo": "bar", "bar": "foo[', '{"foo": "bar", "bar": "foo["}'),
     ('{"foo": "bar", "bar": "foo\\"', '{"foo": "bar", "bar": "foo\\""}'),
+    ('{"foo": "bar", "bar":', '{"foo": "bar", }'),
+    ('{"foo": "bar", ', '{"foo": "bar", }'),
 ]
 
 
